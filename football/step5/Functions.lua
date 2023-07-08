5b76ef7b746a4be4ad0a47cd13ddb9e15cb1f256
rinat <mambetovpost@gmail.com>
2023-07-08 13:06:29 +0300

    +step5

---
+++ football/step5/Functions.lua
@@ -0,0 +1,23 @@
+-- 1
+function myFunction()
+	print("This is my function!")
+end
+-- 2
+function myFunction()
+	print("This is my function!")
+end
+myFunction()
+-- 3
+function myFunction(myMessage)
+	print(myMessage)
+end
+myFunction("Hello there")
+myFunction(123345456)
+myFunction("Final message")
+-- 4
+function add(a,b)
+	print("Adding", a, "+", b)
+	print(a+b)
+end
+add(23,45)
+add(44,56)
